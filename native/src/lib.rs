957dc452ac4690266d0a896deead3c0109d2be23
David Herman <david.herman@gmail.com>
2017-11-03 14:43:57 -0700

    hacky demo of casting an ArrayBuffer to different types

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,16 +1,22 @@
 #[macro_use]
 extern crate neon;
 
+extern crate cslice;
+
 use neon::vm::{Call, JsResult, Lock};
 use neon::js::binary::JsArrayBuffer;
+use cslice::CMutSlice;
+use std::mem;
 
 fn hello(call: Call) -> JsResult<JsArrayBuffer> {
     let scope = call.scope;
     let mut buffer = JsArrayBuffer::new(scope, 8)?;
     buffer.grab(|mut slice| {
-        slice[0] = 17;
-        slice[1] = 42;
-        slice[2] = 111;
+        let len = slice.len();
+        let raw = slice.as_mut_ptr();
+        let mut slice: CMutSlice<f32> = unsafe { CMutSlice::new(mem::transmute(raw), len / 4) };
+        slice[0] = 1.8;
+        slice[1] = 13.4;
     });
     Ok(buffer)
 }
